91e9c506a709cbb245b01d76f3a9eb654642fe20
K-Yevhenii <ya.korniakov@gmail.com>
2025-10-17 17:10:33 +0300

    Created const doctors

---
+++ src/mocks/doctors.ts
@@ -0,0 +1,8 @@
+const doctors = [
+  { name: "Dr. Smith", specialty: "Cardiology" },
+  { name: "Dr. Johnson", specialty: "Dermatology" },
+  { name: "Dr. Lee", specialty: "Pediatrics" },
+  { name: "Dr. Brown", specialty: "Neurology" },
+  { name: "Dr. Davis", specialty: "Orthopedics" },
+];
+export default doctors;
