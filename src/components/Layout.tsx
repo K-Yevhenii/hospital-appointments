c6b02a03fedc1124f9755276373e53e8cba4ff99
K-Yevhenii <ya.korniakov@gmail.com>
2025-10-20 22:54:40 +0300

    add  material-Link, material-AppBar, material-Toolbar, RouterLink, Link-router-dom

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,30 @@
-import { Outlet } from "react-router";
+import Link from "@mui/material/Link";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
+import { Link as RouterLink, Outlet } from "react-router-dom";
+import routes from "../constants/routes";
 
 export default function Layout() {
   return (
     <>
-      Layout
-      <Outlet />
+      <AppBar position="sticky">
+        <Toolbar>
+          <Link
+            color="inherit"
+            component={RouterLink}
+            to={routes.CREATE_APPOINTMENT}
+          >
+            Create
+          </Link>
+          <Link color="inherit" component={RouterLink} to={routes.APPOINTMENTS}>
+            Appointments
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
