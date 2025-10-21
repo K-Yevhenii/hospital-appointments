120d1749f33d1e40277c3351b78b74c38137ecdd
K-Yevhenii <ya.korniakov@gmail.com>
2025-10-21 21:44:56 +0300

    unchanged

---
+++ src/components/DoctorsMultiselect.tsx
@@ -0,0 +1,56 @@
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
+import doctors from "../mocks/doctors";
+import { useState } from "react";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+export default function DoctorsMultiselect() {
+  const [selectedDoctors, setSelectedDoctors] = useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof selectedDoctors>) => {
+    const {
+      target: { value },
+    } = event;
+    setSelectedDoctors(typeof value === "string" ? value.split(",") : value);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="doctors-select-label">Tag</InputLabel>
+        <Select
+          labelId="doctors-select"
+          id="doctors-select"
+          multiple
+          value={selectedDoctors}
+          onChange={handleChange}
+          input={<OutlinedInput label="Tag" />}
+          renderValue={(selected) => selected.join(", ")}
+          MenuProps={MenuProps}
+        >
+          {doctors.map((doctor) => (
+            <MenuItem key={doctor.name} value={doctor.name}>
+              <Checkbox checked={selectedDoctors.includes(doctor.name)} />
+              <ListItemText primary={`${doctor.name} — ${doctor.specialty}`} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
